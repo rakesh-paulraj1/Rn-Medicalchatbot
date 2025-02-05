e43a478fdaee2d5075a6996efc0a03cc9ebcfd5b
Rakesh.P <168630658+rakesh-paulraj1@users.noreply.github.com>
2025-02-05 23:19:45 +0530

    New App

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,24 @@
 import { Stack } from 'expo-router/stack';
 import React from 'react';
+import "../global.css";
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { Drawer } from 'expo-router/drawer';
 
 export default function Layout() {
   return (<>
-  <Stack screenOptions={{headerShown: false}}>
-   
-      <Stack.Screen name="(nobottombar)/account" options={{headerShown: true}}/>
-      </Stack>
+  <GestureHandlerRootView style={{ flex: 1 }}>
+      <Drawer>
+      <Drawer.Screen
+          name="index" // This is the name of the page and must match the url from root
+          options={{
+            drawerLabel: 'Home',
+            title: 'Medical chatbot',
+          }}
+        />
+      </Drawer>
+        
+    </GestureHandlerRootView>
+  
       </>
   );
 }
